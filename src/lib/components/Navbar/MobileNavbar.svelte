e963f310ea6cef7c136117ed1312479a1f99937a
gl0ves <coolhands@protonmail.com>
2025-05-23 01:02:13 +0300

    Fix hydration mismatch

--- src/lib/components/Navbar/MobileNavbar.svelte
+++ src/lib/components/Navbar/MobileNavbar.svelte
@@ -3,6 +3,7 @@
 	import * as Accordion from '$lib/components/ui/accordion/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/button.svelte';
 	import { navItems } from '$lib/components/Navbar/NavItems';
 	import IconMenu from '~icons/mdi/menu';
 	import { goto } from '$app/navigation';
@@ -13,6 +14,7 @@
 	import IconPhone from '~icons/mdi/phone';
 	import IconClose from '~icons/mdi/close';
 	import YpaText from '$lib/components/ui/text/YpaText.svelte';
+	import { cn } from '$lib/utils.js';
 
 	let open = $state(false);
 	let accordionValues = $state<string[]>([]);
@@ -33,14 +35,13 @@
 		/>
 	</a>
 	<Sheet.Root bind:open>
-		<Sheet.Trigger>
-			<Button
-				variant="ghost"
-				size="icon"
-				class="hover:bg-primary-foreground p-6 hover:text-primary active:bg-primary-foreground active:text-white"
-			>
-				<IconMenu class="text-white" />
-			</Button>
+		<Sheet.Trigger 
+			class={cn(
+				buttonVariants({ variant: 'ghost', size: 'icon' }),
+				"hover:bg-primary-foreground p-6 hover:text-primary active:bg-primary-foreground active:text-white"
+			)}
+		>
+			<IconMenu class="text-white" />
 		</Sheet.Trigger>
 		<Sheet.Portal>
 			<Sheet.Overlay />
@@ -56,14 +57,13 @@
 							/>
 						</a>
 					</Sheet.Title>
-					<Sheet.Close>
-						<Button
-							variant="ghost"
-							size="icon"
-							class="hover:bg-primary-foreground p-4 hover:text-primary active:bg-primary-foreground active:text-white"
-						>
-							<IconClose class="h-20 w-20" />
-						</Button>
+					<Sheet.Close
+						class={cn(
+							buttonVariants({ variant: 'ghost', size: 'icon' }),
+							"hover:bg-primary-foreground p-4 hover:text-primary active:bg-primary-foreground active:text-white"
+						)}
+					>
+						<IconClose class="h-20 w-20" />
 					</Sheet.Close>
 				</Sheet.Header>
 				<div class="py-4">
@@ -102,36 +102,50 @@
 					</Accordion.Root>
 				</div>
 				<div class="flex items-center justify-center gap-4 mt-4">
-					<a href="tel:31618472480" target="_blank">
-						<Button variant="outline" size="icon" class="border-white bg-white hover:bg-white">
-							<IconPhone class="h-6 w-6 text-primary" />
-						</Button>
-					</a>
-					<a href="https://wa.me/31618472480" target="_blank">
-						<Button variant="ghost" size="icon" class="border-white bg-whatsapp hover:bg-whatsapp">
-							<IconWhatsApp class="h-6 w-6 text-white" />
-						</Button>
-					</a>
-					<a href="https://www.facebook.com/yourpropertyabroad/" target="_blank">
-						<Button variant="ghost" size="icon" class="border-white bg-facebook hover:bg-facebook">
-							<IconFacebook class="h-6 w-6 text-white" />
-						</Button>
-					</a>
-					<a href="https://www.instagram.com/yourpropertyabroad" target="_blank">
-						<Button
-							variant="ghost"
-							size="icon"
-							class="border-white bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 hover:from-purple-600 hover:via-pink-600 hover:to-orange-600"
-						>
-							<IconInstagram class="h-6 w-6 text-white" />
-						</Button>
-					</a>
+					<Button 
+						href="tel:31618472480"
+						variant="outline" 
+						size="icon" 
+						class="border-white bg-white hover:bg-white"
+					>
+						<IconPhone class="h-6 w-6 text-primary" />
+					</Button>
+					<Button 
+						href="https://wa.me/31618472480"
+						target="_blank"
+						variant="ghost" 
+						size="icon" 
+						class="border-white bg-whatsapp hover:bg-whatsapp"
+					>
+						<IconWhatsApp class="h-6 w-6 text-white" />
+					</Button>
+					<Button 
+						href="https://www.facebook.com/yourpropertyabroad/"
+						target="_blank"
+						variant="ghost" 
+						size="icon" 
+						class="border-white bg-facebook hover:bg-facebook"
+					>
+						<IconFacebook class="h-6 w-6 text-white" />
+					</Button>
+					<Button
+						href="https://www.instagram.com/yourpropertyabroad"
+						target="_blank"
+						variant="ghost"
+						size="icon"
+						class="border-white bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 hover:from-purple-600 hover:via-pink-600 hover:to-orange-600"
+					>
+						<IconInstagram class="h-6 w-6 text-white" />
+					</Button>
 				</div>
 				<Sheet.Footer class="pt-8">
-					<Sheet.Close>
-						<Button size="sm" variant="default" class="bg-white text-black font-semibold"
-							>CLOSE</Button
-						>
+					<Sheet.Close
+						class={cn(
+							buttonVariants({ variant: 'default', size: 'sm' }),
+							"bg-white text-black font-semibold"
+						)}
+					>
+						CLOSE
 					</Sheet.Close>
 				</Sheet.Footer>
 			</Sheet.Content>
